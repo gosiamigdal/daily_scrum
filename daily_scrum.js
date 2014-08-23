1ed76ddbd153d934754efda42156b1e496b3840c
Malgorzata Migdal <malgorzata@migdal.pl>
2014-08-23 16:26:24 -0700

    Can edit but can not save the changes

--- daily_scrum.js
+++ daily_scrum.js
@@ -2,6 +2,8 @@ Task = new Meteor.Collection("task");
 
 
 if (Meteor.isClient) {
+  Session.setDefault('editing_task_item', null);
+
   Template.hello.greeting = function () {
     return "Welcome to daily_scrum.";
   };
@@ -20,17 +22,25 @@ if (Meteor.isClient) {
 
   Template.task.events({
     'click input[type="button"]': function () {
-      var name = $('input[type="text"]').val();
+      var name = $('input[type="text"].new_item').val();
       Task.insert({name: name});
+      Session.set('editing_task_item', null);
     }
   });
 
   Template.task_item.events({
-    "click .remove": function () {
+    'click .remove': function () {
       var id = this._id;
       Task.remove(id);
+    },
+    'dblclick p': function () {
+      Session.set('editing_task_item', this._id);
     }
   });
+
+  Template.task_item.editing = function () {
+    return Session.equals('editing_task_item', this._id);
+  };
 }
 
 if (Meteor.isServer) {

45d3d646680c9aa1a1828666d49606e7a5fef34e
Jacek Migdal <jacek@migdal.pl>
2014-08-23 18:03:22 -0700

    Better task adding
    Add support for time
    
    Can update the time and bugfixes

--- daily_scrum.js
+++ daily_scrum.js
@@ -20,10 +20,45 @@ if (Meteor.isClient) {
     return Task.find().fetch();
   };
 
+  function parseTask(text) {
+    var timeRegex = [
+      "([0-9]{1,2})h ?([0-9]{1,2})m",
+      "([0-9]{1,2})h",
+      "([0-9]{1,2})m"
+    ]
+    var extractTime = [
+      function (r) { return parseInt(r[1]) * 60 + parseInt(r[2]);},
+      function (r) { return parseInt(r[1]) * 60;},
+      function (r) { return parseInt(r[1]);}
+    ]
+
+    for (var i = 0; i < timeRegex.length; i++) {
+      var regex = new RegExp(".*?" + timeRegex[i] + ".*");
+      var result = regex.exec(text);
+      if (result !== null) {
+        return {
+          name: text.replace(new RegExp(timeRegex[i]), ""),
+          minutes: extractTime[i](result)};
+      }
+    }
+
+    return {name: text};
+  }
+
+  function addTask() {
+    var name = $('input[type="text"].new_item').val();
+    Task.insert(parseTask(name));
+    $('input[type="text"].new_item').val("");
+  }
+
   Template.task.events({
+    'keyup .new_item': function () {
+      if (event.which == 13) { // enter key
+        addTask();
+      }
+    },
     'click input[type="button"]': function () {
-      var name = $('input[type="text"].new_item').val();
-      Task.insert({name: name});
+      addTask();
     }
   });
 
@@ -41,7 +76,7 @@ if (Meteor.isClient) {
     'keyup input': function (event) {
       if (event.which === 13) { // enter key
         var value = event.target.value;
-        Task.update(this._id, {$set: {name: value}});
+        Task.update(this._id, {$set: parseTask(value)});
         Session.set('editing_task_item', null);
       }
     },
@@ -52,6 +87,24 @@ if (Meteor.isClient) {
 
   Template.task_item.editing = function () {
     return Session.equals('editing_task_item', this._id);
+  };
+
+  Template.task_item.humanTime = function () {
+    if (Number.isInteger(this.minutes)) {
+      if (this.minutes > 59) {
+        var hours = this.minutes / 60;
+        var minutes = this.minutes % 60;
+        var result = Math.floor(hours) + "h";
+        if (minutes > 0) {
+          result += " " + minutes + "m";
+        }
+        return result;
+      } else {
+        return this.minutes + "m";
+      }
+    } else {
+      return "";
+    }
   };
 }
 

4454622aaf8c0bdc6c4cedf9ef54441291b8d2dd
Jacek Migdal <jacek@migdal.pl>
2014-09-01 14:57:21 -0700

    Show calendar for given month

---
+++ client/calendar.js
@@ -0,0 +1,33 @@
+Template.calendar.month = function() {
+  return moment().format('MMMM');
+}
+
+Template.calendar.weekDays = function() {
+  var result = [];
+  for (var i = 0; i < 7; i++) {
+    var day = moment().day(i).format('ddd');
+    result.push({day: day});
+  }
+  return result;
+}
+
+Template.calendar.weeksOfMonth = function() {
+  var result = [];
+  var m = moment();
+  var currentMonth = m.month();
+  m.date(1);
+  m.day(0);
+  do {
+    var daysOfWeek = [];
+    for (var i = 0; i < 7; i++) {
+      if (m.month() == currentMonth) {
+        daysOfWeek.push({date: m.date()});
+      } else {
+        daysOfWeek.push({date: " "});
+      }
+      m.add(1, "days");
+    }
+    result.push({daysOfWeek: daysOfWeek});
+  } while (m.month() == currentMonth)
+  return result;
+}

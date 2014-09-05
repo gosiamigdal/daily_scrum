8f5230bd5189301ed70c2ce3e01476915da425e0
Jacek Migdal <jacek@migdal.pl>
2014-09-04 21:30:30 -0700

    Can change months in calendar

--- client/calendar.js
+++ client/calendar.js
@@ -1,7 +1,11 @@
 Session.setDefault('current_date', moment().valueOf());
 
+var selectedMoment = function () {
+  return moment(Session.get('current_date'));
+}
+
 Template.calendar.month = function () {
-  return moment().format('MMMM');
+  return selectedMoment().format('MMMM');
 }
 
 Template.calendar.weekDays = function () {
@@ -15,20 +19,21 @@ Template.calendar.weekDays = function () {
 
 Template.calendar.weeksOfMonth = function () {
   var result = [];
-  var m = moment();
-  var currentDay = m.date();
-  var currentMonth = m.month();
+  var today = moment();
+  var m = selectedMoment();
+  var selectedMonth = m.month();
+  var selectedDay = m.date();
   m.date(1);
   m.day(0);
   do {
     var daysOfWeek = [];
     for (var i = 0; i < 7; i++) {
-      if (m.month() == currentMonth) {
+      if (m.month() == selectedMonth) {
         var clazz = "day";
-        if (m.date() > currentDay) {
+        if (m.isAfter(today)) {
           clazz += " future";
         }
-        if (moment(Session.get('current_date')).date() == m.date()) {
+        if (m.date() == selectedDay) {
           clazz += " selected";
         }
         daysOfWeek.push({date: m.date(), attr: {class: clazz}});
@@ -38,14 +43,29 @@ Template.calendar.weeksOfMonth = function () {
       m.add(1, 'days');
     }
     result.push({daysOfWeek: daysOfWeek});
-  } while (m.month() == currentMonth)
+  } while (m.month() == selectedMonth)
   return result;
 }
+
+Template.calendar.events({
+  'click .previous-month': function () {
+    var m = selectedMoment();
+    m.date(1);
+    m.subtract(1, 'month');
+    Session.set('current_date', m.valueOf());
+  },
+  'click .next-month': function () {
+    var m = selectedMoment();
+    m.date(1);
+    m.add(1, 'month');
+    Session.set('current_date', m.valueOf());
+  }
+});
 
 Template.calendarDay.events({
   'click td': function () {
     if (this.date != "") {
-      var m = moment().date(this.date)
+      var m = selectedMoment().date(this.date)
       Session.set('current_date', m.valueOf());
     }
   }

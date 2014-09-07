0fdc6f68c1eed3054a7a0147be2be09a22efd495
Jacek Migdal <jacek@migdal.pl>
2014-09-06 19:47:07 -0700

    Start using iron-router

---
+++ client/done.js
@@ -0,0 +1,173 @@
+Template.hello.groupName = function () {
+  var groupId = Meteor.user().profile.groupId;
+  return Group.findOne(groupId).name;
+}
+
+Template.hello.date = function () {
+  var m = moment(Session.get('current_date'));
+  var prefix;
+  if (m.day() == moment().day() && m.year() == moment().year()) {
+    prefix = "today: ";
+  } else {
+    prefix = "on: ";
+  }
+  return prefix + m.format('MMM Do YYYY');
+}
+
+Template.team.events({
+  'click input[type="button"]': function (e, tmpl) {
+    var email = tmpl.$('input[type="text"]').val();
+    Invitation.insert({
+      email: email,
+      groupId: Meteor.user().profile.groupId,
+      senderUserId: Meteor.userId()
+    });
+  }
+});
+
+function parseTask(text) {
+  var timeRegex = [
+    '([0-9]{1,2})h ?([0-9]{1,2})m',
+    '([0-9]{1,2})h',
+    '([0-9]{1,2})m'
+  ]
+  var extractTime = [
+    function (r) { return parseInt(r[1]) * 60 + parseInt(r[2]);},
+    function (r) { return parseInt(r[1]) * 60;},
+    function (r) { return parseInt(r[1]);}
+  ]
+
+  var m = moment(Session.get('current_date'));
+  var resultTask = {
+    name: text,
+    userId: Meteor.userId(),
+    groupId: Meteor.user().profile.groupId,
+    day: m.date(),
+    month: m.month(),
+    year: m.year()
+  };
+
+  for (var i = 0; i < timeRegex.length; i++) {
+    var regex = new RegExp('.*?' + timeRegex[i] + '.*');
+    var result = regex.exec(text);
+    if (result !== null) {
+      resultTask['name'] = text.replace(new RegExp(timeRegex[i]), '');
+      resultTask['minutesSpent'] = extractTime[i](result);
+      return resultTask;
+    }
+  }
+  return resultTask;
+}
+
+function addTask() {
+  var name = $('input[type="text"].new_item').val();
+  Task.insert(parseTask(name));
+  $('input[type="text"].new_item').val('');
+}
+
+function toHumanTime(rawMinutes) {
+  if (Number.isInteger(rawMinutes)) {
+    if (rawMinutes > 59) {
+      var hours = rawMinutes / 60;
+      var minutes = rawMinutes % 60;
+      var result = Math.floor(hours) + "h";
+      if (minutes > 0) {
+        result += " " + minutes + "m";
+      }
+      return result;
+    } else {
+      return rawMinutes + "m";
+    }
+  } else {
+    return "";
+  }
+}
+
+Template.task.task = function () {
+  var groupId = Meteor.user().profile.groupId;
+  var m = moment(Session.get('current_date'));
+
+  return Task.find({groupId: groupId,
+    day: m.date(),
+    month: m.month(),
+    year: m.year()}).fetch();
+};
+
+Template.task.events({
+  'keyup .new_item': function () {
+    if (event.which == 13) { // enter key
+      addTask();
+    }
+  },
+  'click input[type="button"]': function () {
+    addTask();
+  }
+});
+
+Template.task.humanTotalTime = function () {
+  var sum = 0;
+  var m = moment(Session.get('current_date'));
+  Task.find({day: m.date(),
+      month: m.month(),
+      year: m.year()}).forEach(function (el) {
+    sum += el.minutesSpent || 0;
+  });
+  return toHumanTime(sum);
+};
+
+Template.taskItem.events({
+  'click .remove': function () {
+    var id = this._id;
+    Task.remove(id);
+  },
+  'dblclick p': function (e, tmpl) {
+    Session.set('editing_task_item', this._id);
+    Tracker.flush();
+    var el = tmpl.$(":parent input");
+    el.focus();
+  },
+  'keyup input': function (event) {
+    if (event.which === 13) { // enter key
+      var value = event.target.value;
+      var updatedTask = parseTask(value);
+      delete updatedTask.userId;
+      delete updatedTask.groupId;
+      Task.update(this._id, {$set: updatedTask});
+      Session.set('editing_task_item', null);
+    }
+  },
+  'focusout input': function () {
+    Session.set('editing_task_item', null);
+  }
+});
+
+Template.taskItem.editing = function () {
+  return Session.equals('editing_task_item', this._id);
+};
+
+Template.taskItem.humanTime = function () {
+  return toHumanTime(this.minutesSpent);
+};
+
+Template.taskItem.owner = function () {
+  var owner = Meteor.users.findOne({_id: this.userId});
+  return owner && owner.profile.name;
+};
+
+Template.invitationItem.groupName = function () {
+  var group = Group.findOne(this.groupId);
+  return group.name;
+}
+
+Template.invitationItem.sender = function () {
+  var user = Meteor.users.findOne(this.senderUserId);
+  return user.emails[0].address;
+}
+
+Template.invitationItem.events({
+  'click input[type="button"]': function () {
+    Meteor.users.update(Meteor.userId(), {$set:
+      {"profile.groupId": this.groupId}
+    });
+  }
+});
---
+++ client/setup.js
@@ -0,0 +1,25 @@
+Template.createGroup.events({
+  'click input[type="button"]': function (e, tmpl) {
+    var name = tmpl.$('input[type="text"]').val();
+    console.log("Creating group" + name);
+    var id = Group.insert({name: name, creatorUserId: Meteor.userId()});
+    Meteor.users.update(Meteor.userId(), {$set:
+      {'profile.groupId': id}
+    });
+
+  }
+
+});
+
+Template.joinGroup.hasInvitation = function () {
+  var email = Meteor.user().emails[0].address;
+  if (Invitation.find({email: email}).count() != 0) {
+    return true;
+  }
+}
+
+Template.joinGroup.invitations = function () {
+  // TODO: more emails?
+  var email = Meteor.user().emails[0].address;
+  return Invitation.find({email: email}).fetch();
+}

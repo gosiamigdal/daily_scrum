cffdcc758c3fc5979c98bf9d5fd016ed50e269c4
Jacek Migdal <jacek@migdal.pl>
2014-09-20 21:52:41 -0700

    Add preference to send daily emails

--- client/profileEdit.js
+++ client/profileEdit.js
@@ -6,15 +6,22 @@ Template.profileEdit.username = function () {
   }
 }
 
+Template.profileEdit.sendDailyEmails = function () {
+  if (Meteor.user() != null) {
+    var user = _.defaults(Meteor.user().profile, {sendDailyEmails: true});
+    return user.sendDailyEmails;
+  } else {
+    return false;
+  }
+}
+
 Template.profileEdit.events({
   'click input[type="button"]': function (e, tmpl) {
     var name = tmpl.$('input[type="text"]').val();
+    var sendEmails = tmpl.$('input[type="checkbox"]').prop('checked');
     Meteor.users.update(Meteor.userId(), {$set:
-      {"profile.name": name}
+      {'profile.name': name,
+      'profile.sendDailyEmails': sendEmails}
     });
   }
 });
-
-
-
- 
